4b75148314c026ceba6e626fd0aeb2ada3b64cff
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-08-26 12:15:36 +0200

    Ajout de my_strcpy.c

--- Exo3/src/my_strcpy.c
+++ Exo3/src/my_strcpy.c
@@ -1,7 +1,11 @@
 /// Ecrire une fonction qui copie str1 dans str2
 
 #include <stdio.h>
+#include "my_strlen.h"
 
-void copie(char str1[], char str2[]){
-    
+void copie(char str1[], char str2[]){ // On part tu principe que str1 va rentré dans str2
+    for(int i=0; i<my_strlen(str1); i++){
+        str2[i] = str1[i];
+    }
+    str2[my_strlen(str1)] = '\0';
 }
