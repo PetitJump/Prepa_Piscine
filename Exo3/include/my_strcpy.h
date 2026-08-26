7e7e8a0712d65a9fd8e9986ed4aceba84f3147b4
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-08-26 12:36:33 +0200

    Ajout de la fonction dupliquer

---
+++ Exo3/include/my_strcpy.h
@@ -0,0 +1,1 @@
+void copie(char str1[], char str2[]);
--- Exo3/src/my_strdup.c
+++ Exo3/src/my_strdup.c
@@ -1,1 +1,12 @@
 /// Ecrire une fonction qui alloue puis copie str1 dans str2
+
+#include <stdio.h>
+#include "my_strlen.h"
+#include <stdlib.h> // Pour malloc
+#include "my_strcpy.h"
+
+char* dupliquer(char str1[]){
+    char* str2 = malloc((my_strlen(str1)+1) * sizeof(char)); // +1 car il y a '\0'
+    copie(str1, str2);
+    return str2;
+}
