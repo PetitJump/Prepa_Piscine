f26b2cd068847e0bc0b038f0473e379e542cec05
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-08-26 13:37:50 +0200

    ajout de strcmp

--- Exo3/src/my_strcmp.c
+++ Exo3/src/my_strcmp.c
@@ -1,14 +1,20 @@
-/// Ecrire une fonction qui compare 2 strings, -1 si str1 < str2, 1 si str1 > str2 sinon 0
+/// Ecrire la fonction strcmp()
 
 #include <stdio.h>
 #include "my_strlen.h"
 
 int comparer(char str1[], char str2[]){
-    int taille_str1 = my_strlen(str1);
-    int taille_str2 = my_strlen(str2);
-    if(taille_str1 < taille_str2)
-        return -1;
-    if(taille_str1 > taille_str2)
-        return -1;  
-    return 0;
+    int x;
+    int i = 0;
+    int infini = 1;
+    while(infini == 1){
+        x = str1[i] - str2[i];
+        if(x > 0)
+            return 1;
+        if(x < 0)
+            return -1;
+        if((str1[i] == '\0') && (str2[i] == '\0'))
+            return 0;
+        i++;
+    }
 }
