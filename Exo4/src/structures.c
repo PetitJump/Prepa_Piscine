c4ef6445b7fb5aaa955a01bcc02b60c5e69ff261
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-08-27 22:06:20 +0200

    Correction des struct

--- Exo4/src/structures.c
+++ Exo4/src/structures.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-struct entreprise_t{
+typedef struct {
     char *nom_entreprise;
     int nb_employe;
     char *nom_pdg
-};
+} entreprise_t;
 
-struct statut_t{
+typedef struct {
     char *profession;
     int salaire;
     struct entreprise_t;
 
-};
+} statut_t;
 
-struct info_t{
+typedef struct{
     char *nom;
     char *prenom;
     int age;
     struct statut_t;
-};
+} info_t;
