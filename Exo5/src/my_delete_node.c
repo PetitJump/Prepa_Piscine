bc6482c945e5d71f16cab6088d407632b5f31ee3
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-09-05 10:41:41 +0200

    Fonction delet fonctionel

--- Exo5/src/my_delete_node.c
+++ Exo5/src/my_delete_node.c
@@ -3,6 +3,11 @@
 #include "../include/my.h"
 
 node_t* my_delete_node(int nb, node_t *node){
+    if(node == NULL)
+        return NULL;
+    if(node->values == nb)
+        return node->next;
+    
     if(node->next->values == nb){
         node->next = node->next->next;
         return node;
