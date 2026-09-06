9d366fa918ea58080a461ee22d4b5fe53b15fc43
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-09-07 00:00:14 +0200

    Exo6 fini ! (first try)

--- Exo6/src/my_atoi.c
+++ Exo6/src/my_atoi.c
@@ -1,1 +1,10 @@
 /// Ecrire une fonction qui prend une string en entrée et qui te renvoie un entier : "123" -> 123
+#include <stdio.h>
+
+int my_atoi(char str[]){
+    int rendu = 0;
+    for(int i=0; str[i] != '\0'; i++){
+        rendu = rendu * 10 + (str[i] - '0'); // '0' vaut 48 
+    }
+    return rendu;
+}
--- Exo6/src/my_itoa.c
+++ Exo6/src/my_itoa.c
@@ -1,1 +1,25 @@
 /// Ecrire une fonction qui prend un entier et qui la return une string : 123 -> "123"
+#include <stdio.h>
+#include <stdlib.h>
+
+int how_many_int(int nb){
+    int rendu = 0;
+    while(1){
+        rendu += 1;
+        if(nb / 10 == 0)
+            break;
+        nb = nb / 10;
+    }
+    return rendu;
+}
+
+char* my_itoa(int nb){
+    char *rendu = malloc(BUFSIZ * sizeof(char));
+    int nb_chiffres = how_many_int(nb);
+    for(int i=(nb_chiffres-1); i>=0; i--){
+        rendu[i] = nb % 10 + '0';
+        nb = nb / 10;
+    }
+    rendu[nb_chiffres] = '\0';
+    return rendu;
+}
