937b738938bf2770744f8a04002e67dda948dd50
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-09-13 14:13:19 +0200

    Ajout de my_printf

---
+++ Exo9/include/my.h
@@ -0,0 +1,8 @@
+#ifndef MY_H_
+    #define MY_H_
+
+void my_putstr(char *chr);
+void my_putnbr(int nbr);
+int my_printf(char* str, ...);
+
+#endif
---
+++ Exo9/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/my.h"
+
+int main(void)
+{
+    my_printf("Je m'apelle %s et j'ai %i ans", "killian", 18);
+    return 0;
+}
